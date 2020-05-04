eaf84cff926fc1f14c57c7a6e8583c81cbc636ab
kentaro.sugimoto <kensugi0727@gmail.com>
2020-05-04 05:18:44 +0000

    user login

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_29_041929) do
+ActiveRecord::Schema.define(version: 2020_05_02_134347) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "Name"
+    t.string "name"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
